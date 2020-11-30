7ce316ca36a0baaf85a2d0c74e92c2083c9a2e06
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-11-30 13:12:00 -0500

    chore: commit basics for this year

---
+++ 2020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aoc_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fuelNeeded = 0;
+            fuelNeeded = (12 / 3) - 2;
+
+            Console.WriteLine("Fuel Needed: {0}", fuelNeeded);
+        }
+    }
+}
