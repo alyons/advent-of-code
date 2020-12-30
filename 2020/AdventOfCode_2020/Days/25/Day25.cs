3f824a508c52d54e9f3f47812528ebe9a4e00175
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-12-30 09:18:32 -0500

    chore: work on day 25

--- 2020/AdventOfCode_2020/Days/25/Day25.cs
+++ 2020/AdventOfCode_2020/Days/25/Day25.cs
@@ -6,12 +6,16 @@ namespace AdventOfCode_2020.Days {
   public static class Day25 {
     public static void ReverseCryptographicHandshake() {
       long subjectNumber = 1;
-      long cardPublicKey = 13233401; // 5764801;
-      long doorPublicKey = 6552760; // 17807724;
+      long cardPublicKey = 6552760; // 13233401; // 5764801;
+      long doorPublicKey = 13233401; // 6552760; // 17807724;
       long cardLoopNumber = 0;
       long doorLoopNumber = 0;
       long cardValue = 1;
       long doorValue = 1;
+      long cardEncryptionValue = 1;
+      long doorEncryptionValue = 1;
+
+      bool shouldBreak = false;
 
       do {
         // Iterate Subject Number
@@ -31,26 +35,37 @@ namespace AdventOfCode_2020.Days {
         doorValue = 1;
         doorLoopNumber = 0;
         if (cardValue == cardPublicKey) {
-          Console.WriteLine("Iteration (0): {0}", doorValue);
+          // Console.WriteLine("Iteration (0): {0}", doorValue);
           while(doorValue != doorPublicKey) {
             doorValue = SingleIteration(subjectNumber, doorValue);
             doorLoopNumber++;
-            Console.WriteLine("Iteration ({0}): {1}", doorLoopNumber, doorValue);
+            // Console.WriteLine("Iteration ({0}): {1}", doorLoopNumber, doorValue);
           }
         }
-      } while(cardValue != cardPublicKey && doorValue != doorPublicKey);
+
+        cardEncryptionValue = 1;
+        doorEncryptionValue = 1;
+
+        if (cardValue == cardPublicKey && doorValue == doorPublicKey) {
+          // Logic to validate number
+          for(int i = 0; i < cardLoopNumber; i++) { doorEncryptionValue = SingleIteration(doorPublicKey, doorEncryptionValue); }
+          for(int i = 0; i < doorLoopNumber; i++) { cardEncryptionValue = SingleIteration(cardPublicKey, cardEncryptionValue); }
+        }
+
+        shouldBreak = cardValue == cardPublicKey &&
+                      doorValue == doorPublicKey &&
+                      cardEncryptionValue > 1 &&
+                      cardEncryptionValue == doorEncryptionValue;
+
+        shouldBreak |= subjectNumber >= 20201227;
+
+      } while(!shouldBreak);
 
       Console.WriteLine("Card Subject Number {0} after {1} iterations.", subjectNumber, cardLoopNumber);
       Console.WriteLine("Door Subject Number {0} after {1} iterations.", subjectNumber, doorLoopNumber);
+      Console.WriteLine("Encryption Key: {0}", cardEncryptionValue);
 
-      long encryptionKey = 1;
-      if (cardLoopNumber < doorLoopNumber) {
-        for(int i = 0; i < cardLoopNumber; i++) { encryptionKey = SingleIteration(doorPublicKey, encryptionKey); }
-      } else {
-        for(int i = 0; i < doorLoopNumber; i++) { encryptionKey = SingleIteration(cardPublicKey, encryptionKey); }
-      }
-
-      Console.WriteLine("Encryption Key: {0}", encryptionKey);
+      // 19325929 (too high)
     }
 
     static long SingleIteration(long subjectNumber, long value) {
